12eb445cd85bb493d959b7c0022dd063e4c9cf1d
baegteun <baegteun@gmail.com>
2024-03-21 20:41:39 +0900

    :bento: :: [#1] Sidebar별 Icon 추가

--- src/shared/ui/icons/index.ts
+++ src/shared/ui/icons/index.ts
@@ -1,1 +1,7 @@
 export { default as StellaMusicLogo } from "./stella-music-logo";
+export { default as HomeIcon } from "./home";
+export { default as HomeFillIcon } from "./home-fill";
+export { default as MusicIcon } from "./music";
+export { default as MusicFillIcon } from "./music-fill";
+export { default as FolderIcon } from "./folder";
+export { default as FolderFillIcon } from "./folder-fill";
---
+++ src/shared/ui/icons/music.tsx
@@ -0,0 +1,52 @@
+const MusicIcon = () => (
+  <svg
+    width="33"
+    height="32"
+    viewBox="0 0 33 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_273_6149)">
+      <path
+        d="M23.167 25C25.1 25 26.667 23.433 26.667 21.5C26.667 19.567 25.1 18 23.167 18C21.234 18 19.667 19.567 19.667 21.5C19.667 23.433 21.234 25 23.167 25Z"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7.16699 29C9.09999 29 10.667 27.433 10.667 25.5C10.667 23.567 9.09999 22 7.16699 22C5.234 22 3.66699 23.567 3.66699 25.5C3.66699 27.433 5.234 29 7.16699 29Z"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M26.667 10L10.667 14"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10.667 25.5V8L26.667 4V21.5"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_273_6149">
+        <rect
+          width="32"
+          height="32"
+          fill="white"
+          transform="translate(0.666992)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default MusicIcon;
