06c861cda746f9c00ca7c48f29db288a3a90ca7f
baegteun <baegteun@gmail.com>
2024-03-22 22:36:26 +0900

    :lipstick: :: [#1] Icons / svg의 hypen 모두 제거

--- src/shared/ui/icons/music-fill.tsx
+++ src/shared/ui/icons/music-fill.tsx
@@ -6,36 +6,36 @@ const MusicFillIcon = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_273_6077)">
+    <g clipPath="url(#clip0_273_6077)">
       <path
         d="M23.165 25.0002C25.098 25.0002 26.665 23.4332 26.665 21.5002C26.665 19.5672 25.098 18.0002 23.165 18.0002C21.232 18.0002 19.665 19.5672 19.665 21.5002C19.665 23.4332 21.232 25.0002 23.165 25.0002Z"
         fill="white"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M7.16504 29.0002C9.09804 29.0002 10.665 27.4332 10.665 25.5002C10.665 23.5672 9.09804 22.0002 7.16504 22.0002C5.23204 22.0002 3.66504 23.5672 3.66504 25.5002C3.66504 27.4332 5.23204 29.0002 7.16504 29.0002Z"
         fill="white"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M26.665 10.0002L10.665 14.0002"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M10.665 25.5002V8.00024L26.665 4.00024V21.5002"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <rect
         x="9.99805"
--- src/shared/ui/icons/music.tsx
+++ src/shared/ui/icons/music.tsx
@@ -6,34 +6,34 @@ const MusicIcon = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0_273_6149)">
+    <g clipPath="url(#clip0_273_6149)">
       <path
         d="M23.167 25C25.1 25 26.667 23.433 26.667 21.5C26.667 19.567 25.1 18 23.167 18C21.234 18 19.667 19.567 19.667 21.5C19.667 23.433 21.234 25 23.167 25Z"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M7.16699 29C9.09999 29 10.667 27.433 10.667 25.5C10.667 23.567 9.09999 22 7.16699 22C5.234 22 3.66699 23.567 3.66699 25.5C3.66699 27.433 5.234 29 7.16699 29Z"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M26.667 10L10.667 14"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M10.667 25.5V8L26.667 4V21.5"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </g>
     <defs>
