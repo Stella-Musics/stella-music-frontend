2f170ae306320b19b5f4ef374e47ffaa47d591bc
baegteun <baegteun@gmail.com>
2024-03-24 18:37:16 +0900

    :lipstick: :: [#5] widgets / music / MusicList UI 추가

---
+++ src/widgets/music-list/ui/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const MusicListContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: ${({ theme }) => theme.color.background};
+  padding: 90px 90px 0 90px;
+`;
