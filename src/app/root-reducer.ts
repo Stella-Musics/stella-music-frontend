451a76f6e85e4c21b9ee5d48c5a761a5b3767c7b
baegteun <baegteun@gmail.com>
2024-03-24 18:35:04 +0900

    :sparkles: :: [#5] entities / music / Music Slice 추가

--- src/app/root-reducer.ts
+++ src/app/root-reducer.ts
@@ -1,6 +1,8 @@
+import { musicSlice } from "@/entities/music";
 import { sidebarSlice } from "@/entities/sidebar";
 import { combineReducers } from "@reduxjs/toolkit";
 
 export const rootReducer = combineReducers({
   [sidebarSlice.name]: sidebarSlice.reducer,
+  [musicSlice.name]: musicSlice.reducer,
 });
